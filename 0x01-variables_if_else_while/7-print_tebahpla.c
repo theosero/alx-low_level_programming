20112dd03985b6844ebb03294d6296016e0079ce
theosero <sirfesto@gmail.com>
2022-09-09 13:47:26 -0700

    variables if else while

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: always 0 (Success)
+ */
+
+int main(void)
+{
+	char alphabet;
+
+	for (alphabet = z; albhabet >= a; alphabet--)
+		putchar(alphabet);
+	putchar('\n');
+	return (0);
+}
