2ccb37aff56f1496638b5a084ba6a96387ec8917
theosero <sirfesto@gmail.com>
2022-09-18 22:30:40 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -10,9 +10,10 @@ void print_alphabet_x10(void)
 {
 	int i = 0;
 
-	while (i <10)
+	while (i < 10)
 	{
 		char i = 'a';
+		
 		while (i <= 'z')
 		{
 			_putchar(i);
@@ -20,6 +21,6 @@ void print_alphabet_x10(void)
 		}
 		i++;
 		_putchar('\n');
-		return ;
+		return;
 	}
 }
