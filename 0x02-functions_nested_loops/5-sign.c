f681f23055fbaf9d8ae594011203c7bb268060bb
theosero <sirfesto@gmail.com>
2022-09-19 00:35:52 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_sign - entry point
+ * @n: passed parameter
+ *
+ * Return: always zero
+ */
+
+int print_sign(int n)
+{
+	if (n >= 0)
+	{
+		_putchar('+');
+		return (1);
+	}
+	if (n == 0)
+	{
+		_putchar(0);
+		return (0);
+	}
+	if (n <= 0)
+	{
+		_putchar('-');
+		return -1;
+	}
+	return (0);
+}
