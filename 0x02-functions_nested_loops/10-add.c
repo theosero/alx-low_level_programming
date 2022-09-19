f132ae25408045432e6dbf567e73e047663c5102
theosero <sirfesto@gmail.com>
2022-09-19 03:18:33 -0700

    functions and nested loops

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -3,14 +3,14 @@
 /**
  * add - entry point
  * @a: first int
- * @b; second int
+ * @b: second int
  *
  * Return: sum of two ints
  */
 
 int add(int a, int b)
 {
-	int sum;
-	sum = a + b;
+	int sum = a + b;
+	
 	return (sum);
 }
