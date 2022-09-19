28433608e78d591eef10c4b6b01b4fa0c9f7497e
theosero <sirfesto@gmail.com>
2022-09-19 03:14:25 -0700

    functions and nested loops

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * add - entry point
+ * @a: first int
+ * @b; second int
+ *
+ * Return: sum of two ints
+ */
+
+int add(int a, int b)
+{
+	int sum;
+	sum = a + b;
+	return (sum);
+}
