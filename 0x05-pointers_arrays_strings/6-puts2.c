7c3aa3aef489b147411b982c1fa66c7e8e9aeca6
theosero <sirfesto@gmail.com>
2022-09-20 13:45:50 -0700

    pointers, arrays and strings

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * puts2 - entry point
+ *
+ * @str: passed param
+ */
+
+void puts2(char *str)
+{
+	int i = 0;
+
+	for (i = 0; *str; str ++)
+	{
+		if (i++ % 2 == 0)
+		{
+			_putchar(*str);
+		}
+	}
+	_putchar('\n');
+}
