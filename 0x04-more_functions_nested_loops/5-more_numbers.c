b7841d45023d2aa3c9c6a81f2385e277cfcf79c8
theosero <sirfesto@gmail.com>
2022-09-29 23:38:10 -0700

    more functions and loops

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -5,20 +5,17 @@
  *
  * Return: always zero
  */
-
 void more_numbers(void)
 {
-	int i, j;
+	int num, count;
 
-	for (i = 0; i < 10; i++)
+	for (count = 0; count <= 9; count++)
 	{
-		for (j = 0; j <= 14; j+++)
+		for (num = 0; num <= 14; num++)
 		{
-			if (b >= 10)
-			{
-				_putchar(b / 10 + '0');
-			}
-			_putchar(b % 10 + '0');
+			if (num > 9)
+				_putchar((num / 10) + '0');
+			_putchar((num % 10) + '0');
 		}
 		_putchar('\n');
 	}
