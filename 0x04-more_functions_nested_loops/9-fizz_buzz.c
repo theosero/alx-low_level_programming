2f2fc256dd49878f7bea56db865b0569ae7fe28c
theosero <sirfesto@gmail.com>
2022-09-30 00:54:23 -0700

    more functions and loops

---
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - entry point
+ *
+ * Return: always 0
+ */
+
+void main(void)
+{
+	int i = 0;
+	
+	for (i = 1; i <= 100; i++)
+    	{
+        	if (i % 3 == 0 && i % 5 == 0)
+        	{
+            		printf("FizzBuzz ");
+        	}	
+        	else if (i % 3 == 0)
+        	{
+        		printf("Fizz ");
+        	}
+        	else if (i % 5 == 0)
+        	{
+			printf("Buzz ");
+        	}
+        	else
+        	{
+			printf("%d ", i);
+        	}
+	}
+}
