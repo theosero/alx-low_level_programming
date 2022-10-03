6bcd7c17e090f70474f1d7d6e4c5aafc0e845b02
theosero <sirfesto@gmail.com>
2022-10-03 14:39:01 -0700

    more pointers

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strcat - entry point
+ * @dest: passed parameter
+ * @src: passed parameter
+ * Return: return 
+ */
+
+char *_strcat(char *dest, cahr *src)
+{
+	int i, j = 0;
+
+	while (dest[i] != '\0')
+	{
+		i++;
+	}
+	while (src[j] != '\0')
+	{
+		dest[i] += src[j];
+		i++;
+		j++;
+	}
+	dest += '\0';
+	retur (dest);
+}
