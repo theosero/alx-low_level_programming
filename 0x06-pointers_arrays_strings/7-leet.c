ee0e95b627e7bfc1b038b7ee1862d74b324fbcac
theosero <sirfesto@gmail.com>
2022-10-03 16:36:54 -0700

    debugging

---
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -0,0 +1,32 @@
+#include "main.c"
+
+/**
+ * leet - entrypoint
+ * @str: passed parameter
+ * Return: akways zero
+ */
+
+char *leet(char *str)
+{
+	char *letters, *replace;
+	int count, count2;
+
+	letters = "aAeEoOtTlL";
+	replace = "4433007711";
+
+	count = 0;
+	while (str[count] != '\0')
+	{
+		count2 = 0;
+		while (letters[count2] != '\0')
+		{
+			if (str[count] == letters[count2])
+			{
+				str[count] = replace[count2];
+			}
+			count2++;
+		}
+		count++;
+	}
+	return (str);
+}
