cea5056cd3c8d8948e0e9b295a2208511df67a7a
theosero <sirfesto@gmail.com>
2022-10-04 21:59:32 -0700

    malloc and free

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * _strdup - entry point
+ * @str: passed string
+ * Return: always 0
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *prt;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	prt = malloc(sizeof(char)  * i+1);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	j = 0;
+	while (str[j] != '\0')
+	{
+		ptr[j] = str[j];
+		j++;
+	}
+	prt[j] = '\0';
+	return (prt);
+}
