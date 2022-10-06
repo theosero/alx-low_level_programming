635aa0f65e908f4f39da851a43587a4988136dca
theosero <sirfesto@gmail.com>
2022-10-06 13:09:56 -0700

    more on malloc and free

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * malloc_checked - entry point
+ * @b: amount of bytes to be allocated
+ * Return: return a pointer if okay, 98 if error
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr = malloc(b);
+	
+	if (ptr == NULL)
+		exit(98);
+	return (ptr);
+}
