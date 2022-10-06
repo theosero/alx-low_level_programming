bfd8bbc1e395361aa5cfe4945b5bcf940f2bf0d5
theosero <sirfesto@gmail.com>
2022-10-06 01:30:54 -0700

    argc and argv

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * main- entry point
+ * @argc: parameter no
+ * @argv: passed parameters
+ * Return: Success 0, error 1
+ */
+
+int main(int argc, char *argv[])
+{
+	int mul;
+	if (argc ==3)
+	{
+		mul = atoi(argv[1] * argv[2]);
+		printf("%d\n", mul);
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
