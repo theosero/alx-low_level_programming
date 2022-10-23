8f9e150cb575c8ff1b55c3dc7b94062eda34b2c4
theosero <sirfesto@gmail.com>
2022-10-23 05:57:50 -0700

    more pointers,string, array

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * _strchr - locate character in a string
+ * @s: string to search
+ * @c: cgaracter to find
+ * Return: pointer to the char
+ */
+char *_memcpy(char *dest, char *src, unsigned int n)
+{
+	int i = 0;
+
+	while(s[i] != '\0')
+	{
+		i++;
+		if (s[i] == c)
+		{
+			return (&s[i]);
+		}
+	}
+	return ('\0');
+}
