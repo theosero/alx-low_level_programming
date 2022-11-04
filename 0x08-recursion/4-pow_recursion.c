196fe917015f8147a523a5da3fa31f188d23a971
theosero <sirfesto@gmail.com>
2022-11-04 06:28:34 -0700

    recursion

---
+++ 0x08-recursion/4-pow_recursion.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * _pow_recursion - return exponent of a number
+ * @x: first parameter
+ * @y: second parameter
+ * Return: exponent of x raised to y
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (1)
+	}
+	else
+	{
+		return (x * _pow_recursion(x, y - 1));
+	}
+}
